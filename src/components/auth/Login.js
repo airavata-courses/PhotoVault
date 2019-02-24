e9a52bbe180508c058f50be1582e6a2853a833b4
Vrishali Bhor <vbhor@iu.edu>
2019-02-24 18:16:04 -0500

    Testing CD Jenkins 3

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -59,7 +59,7 @@ class Login extends Component {
           <div className="container">
             <div className="row">
               <div className="col-md-8 m-auto">
-                <h1 className="display-4 text-center">Log In --test</h1>
+                <h1 className="display-4 text-center">Log In --test3</h1>
                 <p className="lead text-center">
                   Sign in to your PhotoVault account
                 </p>
