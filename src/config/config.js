d92ed95712ea7693fbfbc6b0f248655669d8c060
Vrishali Bhor <vbhor@iu.edu>
2019-02-28 10:29:43 -0500

    Change connection urls

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,8 @@
-const AUTH_URL = 'http://localhost:5000';
-const SEARCH_URL = 'http://localhost:5000';
-const EXPLORE_URL = 'http://localhost:8080';
-const UPLOAD_URL = 'http://localhost:5000';
-const PICTURE_DET_URL = 'http://localhost:8000';
+const AUTH_URL = 'http://149.165.156.42:5000';
+const SEARCH_URL = 'http://149.165.156.42:5000';
+const EXPLORE_URL = 'http://149.165.156.42:8080';
+const UPLOAD_URL = 'http://149.165.156.42:5000';
+const PICTURE_DET_URL = 'http://149.165.156.42:8000';
 module.exports = {
   search: SEARCH_URL,
   upload: UPLOAD_URL,
