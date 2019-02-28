6a64179e05e73b1abd823970dd006854b47846c8
Vrishali Bhor <vbhor@iu.edu>
2019-02-28 10:39:18 -0500

    Resolve cors issue

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const users = require("./routes/api/users");
-const search = require("./routes/api/fileOps");
-const uploadImg = require("./routes/api/uploadImg");
-const passport = require("passport");
-const cors = require("cors");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const users = require('./routes/api/users');
+const search = require('./routes/api/fileOps');
+const uploadImg = require('./routes/api/uploadImg');
+const passport = require('passport');
+const cors = require('cors');
 
 //DB connection
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI;
 
 //Connect to DB
 mongoose
   .connect(db)
-  .then(() => console.log("Connected to database"))
+  .then(() => console.log('Connected to database'))
   .catch(err => console.log(err));
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!!!!!!!!!!!"));
-app.get("/test", (req, res) => res.json({ msg: "hello" }));
+app.get('/', (req, res) => res.send('Hello World!!!!!!!!!!!'));
+app.get('/test', (req, res) => res.json({ msg: 'hello' }));
 //Use routes
 
 const port = 5000;
@@ -30,12 +30,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
 
 //passport COnfig
-require("./config/passport")(passport);
+require('./config/passport')(passport);
 
 // parse application/json
 app.use(bodyParser.json());
 // app.use(cors());
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: '*' }));
 
 // app.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
@@ -48,9 +48,9 @@ app.use(cors({ origin: "http://localhost:3000" }));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-app.use("/api/users", users);
-app.use("/api/fileOps", search);
-app.use("/api/uploadImg", uploadImg);
+app.use('/api/users', users);
+app.use('/api/fileOps', search);
+app.use('/api/uploadImg', uploadImg);
 
 // Then use it before your routes are set up:
 
