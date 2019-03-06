6e56ea3ee688234885bac1cc5fa2cea7fb69f58c
Vrishali Bhor <vbhor@iu.edu>
2019-03-06 17:32:03 -0500

    Api Gateway Upload

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ const AUTH_URL = 'http://149.165.156.42:5000';
 const SEARCH_URL = 'http://149.165.156.42:5000';
 const EXPLORE_URL = 'http://149.165.156.42:5000';
 const UPLOAD_URL = 'http://149.165.156.42:5000';
-const PICTURE_DET_URL = 'http://149.165.156.42:8000';
+const PICTURE_DET_URL = 'http://149.165.156.42:5000';
 module.exports = {
   search: SEARCH_URL,
   upload: UPLOAD_URL,
