871014adc0fbedfc3bfe6edcedaaa5d9ae68c357
uteerna <uteernak@gmail.com>
2019-03-24 00:07:04 +0000

    server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.post("/", (request, response) => {
 
 		var options = {
 
-			hostname: "149.165.156.42:5000",
+			hostname: "http://149.165.156.42:5000",
 			port: 5000,
 			path: "/api/users/login",
 			method: "POST",
