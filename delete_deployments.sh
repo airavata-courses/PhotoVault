a26b63d36329d9fc3eccd68f148bfbc0c995e169
Uteerna <uteernak@gmail.com>
2019-03-31 03:48:59 +0000

    K8s master server files

--- delete_deployments.sh
+++
@@ -1,6 +0,0 @@
-kubectl delete deployment website-deployment loginsearch explore upload-deployment
-kubectl delete service website loginsearch explore upload-services
-kubectl apply -f node.yaml
-kubectl apply -f website_node.yaml
-kubectl apply -f explore_java.yaml
-kubectl apply -f python.yaml
