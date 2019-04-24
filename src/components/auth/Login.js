b195a070acde0afb7a7357a9a6a5908143baa497
Vrishali Bhor <vbhor@iu.edu>
2019-04-24 11:18:34 -0400

    Testing

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -101,7 +101,7 @@ class Login extends Component {
           <div className="container">
             <div className="row">
               <div className="col-md-8 m-auto">
-                <h1 className="display-4 text-center">Log In</h1>
+                <h1 className="display-4 text-center">Log In _ Blue</h1>
                 <p className="lead text-center">
                   Sign in to your PhotoVault account
                 </p>
