08d125ed0a638b4cf5bad6fba0a27728d8f6874a
Murtaza Khambaty <mkhambat@iu.edu>
2019-04-28 20:47:08 +0000

    Add test cases

--- exploreImages/src/test/java/net/codejava/exploreImages/ExploreImagesApplicationTests.java
+++ exploreImages/src/test/java/net/codejava/exploreImages/ExploreImagesApplicationTests.java
@@ -1,17 +1,33 @@
 package net.codejava.exploreImages;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class ExploreImagesApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	public void contextLoads() {
+	public void exploreImagesTest() throws Exception {
+
+		this.mockMvc.perform(get("/explore/true")).andExpect(status().isOk());
+
+
 	}
-
 }
-
