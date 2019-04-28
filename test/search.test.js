d950bc6dce1ab84ae0b0b2141c6b14dcad4914ea
Murtaza Khambaty <mkhambat@iu.edu>
2019-04-28 19:45:32 -0400

    Edited test cases

--- test/search.test.js
+++ test/search.test.js
@@ -1,21 +1,16 @@
-const request = require('supertest');
-var test = require('tape');
-const express = require('express');
-var assert = require('assert');
+const request = require("supertest");
+var expect = require("chai").expect;
+const express = require("express");
 
-describe('request(url)', function () {
-
-  const app = express();
-
-  test('testing search', function (t) {
-
-    request(app)
-      .get('/api/fileOps/searchString')
-      .expect('Content-Type', /json/)
+describe("testDietURL()", function() {
+  it("should return diet data", function() {
+    // 1. ARRANGE
+    request(express)
+      .get("/diet")
+      .expect("Content-Type", /json/)
       .expect(200)
-      .end(function (err, res) {
+      .end(function(err, res) {
         t.end();
       });
-  })
-
+  });
 });
